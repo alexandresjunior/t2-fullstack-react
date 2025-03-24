4c236264f5aef92f21dfbb48a0d5e0e4ba819c20
Alexandre Junior <souzajr.alexandre@gmail.com>
2025-03-24 19:34:20 -0300

    Criacao da pagina 404

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Login from './paginas/Login';
 import './App.css';
 import Cadastro from './paginas/Cadastro';
+import Pagina404 from './paginas/Pagina404';
 
 function App() {
   return (
     // <Login />
-    <Cadastro />
+    // <Cadastro />
+    <Pagina404 />
   );
 }
 
